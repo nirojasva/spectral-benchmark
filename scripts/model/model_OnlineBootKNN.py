9c350ff673aa5c284e045cf7fcbb76a8fcd43fdd
nirojasva <nirojasva@unal.edu.co>
2026-05-05 00:34:32 +0200

    displaying std from experiments

--- scripts/model/model_OnlineBootKNN.py
+++ scripts/model/model_OnlineBootKNN.py
@@ -478,9 +478,9 @@ if __name__ == "__main__":
     # Go two level up
     current_dir = current_dir.parent.parent
 
-    #DATA_PATH = current_dir / 'datasets' / 'raw' / 'ScenariosV4'
+    DATA_PATH = current_dir / 'datasets' / 'raw' / 'ScenariosV4_lite'
     #DATA_PATH = current_dir / 'datasets' / 'raw' / 'ScenariosV3'
-    DATA_PATH = current_dir / 'datasets' / 'raw'
+    #DATA_PATH = current_dir / 'datasets' / 'raw'
     PATH_PLOT_FILE_NAME_INTERPRETATION = current_dir / 'notebooks' / 'img_anomalies'
     PATH_PLOT_FILE_NAME_SCORE = current_dir / 'notebooks' / 'img_monitoring_score'
 
@@ -494,7 +494,7 @@ if __name__ == "__main__":
     # Results dataframe
     summary_data = []
 
-    NUMBER_RUNS = 1
+    NUMBER_RUNS = 3
     WINDOW_SIZE = 60
     MODEL = "Tested Model"
     TRANF = "ZNORM"
@@ -507,10 +507,10 @@ if __name__ == "__main__":
     ALPHA = 0.05
     SLEEP_TIME = 0
     SCORE_DIR = "direct" #direct or inverse
-    DATASETS_LIST = ["A1_","A2_","A3_","A4_","A5_","A6_","A7_","A8_","A9_"]
+    #DATASETS_LIST = ["A1_","A2_","A3_","A4_","A5_","A6_","A7_","A8_","A9_"]
     
-    #DATASETS_LIST = ["A6_"]
-    #DATASETS_LIST = ["DA1_", "SA1_", "TA1_","DA2_", "SA2_", "TA2_","DA3_", "SA3_", "TA3_"]
+    #DATASETS_LIST = ["A1_"]
+    DATASETS_LIST = ["DA1_", "SA1_", "TA1_","DA2_", "SA2_", "TA2_","DA3_", "SA3_", "TA3_"]
     MIN_Z_SCORE = 4
     #REGION_STUDY = ["386.45:393.38:N2", "773.38:780.40:O2","652.47:659.53:H","304.46:311.54:OH","748.38:752.19:Ar"] 
     REGION_STUDY = ["386.45:393.38:N2", "773.38:780.40:O2","652.47:659.53:H","304.46:311.54:OH"] 
@@ -544,9 +544,9 @@ if __name__ == "__main__":
 
             row = 0
             #learner = OnlineBootKNN(schema=schema, window_size=WINDOW_SIZE, chunk_size=CHUNCK_SIZE,  ensemble_size=ENSEMBLE_SIZE, dmetric=DMETRIC, transf=TRANF, alpha=ALPHA, algorithm=ALGO, no_bootstrapp=NO_BOOTSTRAPP, no_z_score=NO_ZSCORE, random_seed=iter)
-            learner = HStreeCapy(schema=schema, window_size=60, number_of_trees=25, anomaly_threshold=0.5, size_limit=0.1, max_depth=15, random_seed=1)
+            #learner = HStreeCapy(schema=schema, window_size=60, number_of_trees=25, anomaly_threshold=0.5, size_limit=0.1, max_depth=15, random_seed=1)
             #learner = OnlineIsolationForest(schema=schema, window_size=240, random_seed=1, growth_criterion ='adaptive', max_leaf_samples=32, n_jobs= -1, num_trees=32)
-            #learner = IForestASD(window_size=240, initial_window_X= None)
+            learner = IForestASD(window_size=240, initial_window_X= None)
             #learner = KitNet(hidden_ratio=0.75, learning_rate=0.1, max_size_ae=10, grace_feature_mapping=240, grace_anomaly_detector=240)
             #learner = ExactStorm(window_size=120, max_radius=0.1)
             #learner = RobustRandomCutForest(shingle_size=240, num_trees=4, tree_size=256) 
@@ -619,6 +619,21 @@ if __name__ == "__main__":
     # Sample pivot table (replace this with your pivot table)
     pivot = summary_data.pivot_table(values=[ "AUC_PR"],
                                         columns=['scenario'], index=['method'], aggfunc='mean')
+
+    # Adding a "Total" row
+    pivot['Avg'] = pivot.mean(axis=1)  # Row-wise mean, can use sum(axis=1) for total sum
+
+    # Rounding the pivot table values to 3 decimal places for better readability
+    pivot = pivot.round(3)
+
+    # Sorting the pivot table by the "Avg" column in descending order
+    pivot = pivot.sort_values(by='Avg', ascending=False)
+
+    # Display the sorted pivot table
+    print(pivot)
+    
+    pivot = summary_data.pivot_table(values=[ "AUC_PR"],
+                                        columns=['scenario'], index=['method'], aggfunc='std')
 
     # Adding a "Total" row
     pivot['Avg'] = pivot.mean(axis=1)  # Row-wise mean, can use sum(axis=1) for total sum
